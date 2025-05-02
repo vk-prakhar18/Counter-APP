14990d95b307b5f352963d383b943d3bdecf79b1
Prakhar Bhatnagar <Bhatnagar.prakhar2009@gmail.com>
2025-05-02 06:40:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let count = 0;
+const counting = document.getElementById('counting');
+
+document.getElementById('increase').addEventListener('click', () => {
+    count++;
+    counting.textContent = count;
+});
+
+document.getElementById('decrease').addEventListener('click', () => {
+    count--;
+    counting.textContent = count;
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+    count = 0;
+    counting.textContent = count;
+});
